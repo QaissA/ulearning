98559437bcd135a37d9264f6ca1f356263e210ab
adam-nouina <adam.moi96@gmail.com>
2025-02-23 13:15:32 -0800

    systeme gestion classes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,7 @@ import studentsRouter from "./studentRoutes";
 import attendanceRouter from "./attendanceRoutes";
 import notesRouter from "./noteRoutes";
 import matiereRouter from "./matiereRoutes";
+import classRouter from "./classRoutes";
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.use('/students', studentsRouter);
 router.use('/attendance', attendanceRouter);
 router.use('/notes', notesRouter);
 router.use('/matieres', matiereRouter);
+router.use('/classes', classRouter);
 
 export default router;
